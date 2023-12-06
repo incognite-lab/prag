8488b26c79f6c6c23ea2b26930a7c00c8f50a3e1
radoskov <radoslav.skoviera@cvut.cz>
2023-12-06 19:52:39 +0100

    Testing of reworked structure

--- tests/test_simple_run.py
+++ tests/test_simple_run.py
@@ -1,15 +1,22 @@
-import numpy as np
-
 import os
 import sys
+from typing import Any, Optional
+
+import numpy as np
+
 SCRIPT_DIR = os.path.dirname(__file__)
 MODULE_DIR = os.path.abspath(os.path.join(SCRIPT_DIR, os.pardir))
 sys.path.append(MODULE_DIR)
 
-from rddl.entity import Variable, LocationType, Gripper, ObjectEntity, Entity
-from rddl.predicate import Predicate
-from rddl.reward import Reward
+from rddl import Entity, Operand, Predicate, Reward, Variable
 from rddl.action import AtomicAction
+from rddl.entity import Gripper, Location, LocationType, ObjectEntity
+from rddl.predicate import IsHolding, Near
+from rddl.reward import NearReward
+
+"""
+ The following function and variables should be defined for a specific environment
+"""
 
 
 def euclidean_distance(point_A: np.ndarray, point_B: np.ndarray) -> float:
@@ -17,49 +24,97 @@ def euclidean_distance(point_A: np.ndarray, point_B: np.ndarray) -> float:
 
 
 def is_holding(gripper, obj) -> bool:
-    return gripper.is_holding(obj)
+    return np.linalg.norm(gripper.location - obj.location) < NEAR_THRESHOLD
 
+
+NEAR_THRESHOLD = 0.1
 
 functions = {
     "is_holding": is_holding,
-    "euclidean_distance": euclidean_distance
+    "euclidean_distance": euclidean_distance,
+    "near_threshold": NEAR_THRESHOLD,
 }
 
 # CONSTANTS
-NEAR_THRESHOLD = 0.01
 
-Entity.set_observation_getter(lambda : {
-
-})
-
-class Near(Predicate):
-    _EDISTANCE_PREDICATE = functions["euclidean_distance"]
-
-    def __init__(self, object_A: Variable[LocationType], object_B: Variable[LocationType]) -> None:
-        self.object_A = object_A
-        self.object_B = object_B
-
-    def decide(self):
-        print("Checking near predicate")
-        return Near._EDISTANCE_PREDICATE(self.object_A.location, self.object_B.location) < NEAR_THRESHOLD
-
-    def evaluate(self):
-        print("Evaluating near predicate")
-        return Near._EDISTANCE_PREDICATE(self.object_A.location, self.object_B.location) - NEAR_THRESHOLD
+# END OF CUSTOM FUNCTIONS & VARIABLES
 
 
-class NearReward(Near, Reward):
-    pass
+class EnvObjectProxy:  # This should be replaced by EnvObject from myGym
+
+    def __init__(self, name: str):
+        self.name = name
+        self._position = np.random.randn(3)
+        self._orientation = np.random.randn(3)
+
+    def set_position(self, position: np.ndarray):
+        self._position = position
+
+    def set_orientation(self, orientation: np.ndarray):
+        self._orientation = orientation
+
+    def get_position(self) -> np.ndarray:
+        return self._position
+
+    def get_orientation(self) -> np.ndarray:
+        return self._orientation
+
+    def get_position_and_orientation(self) -> tuple[np.ndarray, np.ndarray]:
+        return self.get_position(), self.get_orientation()
+
+    def get_name(self) -> str:
+        return self.name
+
+    def get_uuid(self) -> str:
+        return self.name + "_uuid"
+
+
+class EnvSimulator:  # This should be replaced by actual env from myGym
+    """Simulates the simulation environment"""
+
+    def __init__(self, list_of_objects: list[str]):
+        self._objects = [EnvObjectProxy(obj) for obj in list_of_objects]
+
+    def reset(self):
+        pass
+
+    def step(self, action):
+        # generate observation
+        obs = {}
+        for obj in self._objects:
+            obj.set_position(np.random.randn(3))  # randomly jiggle the object about
+            obj.set_orientation(np.random.randn(3))
+            obs[obj.get_name()] = obj
+        return obs
+
+
+class Observer:  # This class serves merely as a container & memory for current observation
+
+    def __init__(self):
+        self.obs = None
+
+    def set_observation(self, obs):
+        self.obs = obs
+
+    def get_observation(self):
+        return self.obs
+
+
+Location.monkey_patch(Location._get_location, lambda self: self().get_position())
 
 
 class Apple(ObjectEntity):
 
-    def __init__(self, kind: str = "RedDelicious"):
-        super().__init__(self._get_generic_reference(), "apple")
+    def __init__(self, reference: Optional[str] = None, kind: str = "RedDelicious"):
+        super().__init__(self._get_generic_reference() if reference is None else reference, "apple")
         self._kind = kind
 
-    def _get_location(self):
-        return np.random.randn(3)
+
+class TiagoGripper(Gripper):
+
+    def __init__(self, reference: Optional[str] = None):
+        super().__init__("gripper_tiago" if reference is None else reference)
+        self._is_holding_predicate = IsHolding(self)
 
 
 class Reach(AtomicAction):
@@ -72,34 +127,41 @@ class Reach(AtomicAction):
         self._reward = NearReward(self._g, self._o)
 
 
-class TiagoGripper(Gripper):
-
-    def __init__(self):
-        super().__init__("gripper_tiago")
-
-    def _is_holding(self, obj: ObjectEntity) -> bool:
-        return obj.name == "apple"
-
-    def _get_location(self):
-        return np.random.randn(3)
-
-
 if __name__ == "__main__":
-    # ve = Variable("test_ve", ObjectEntity)
+    Operand.set_mapping(functions)
+
+    gripper_name = "tiago_gripper"
+    apple_name = "apple_01"
     g1 = Variable("gripper", Gripper)
     o1 = Variable("apple", ObjectEntity)
+    print(f"gripper: {g1}")
 
     r = Reach(g1, o1)
 
+    env = EnvSimulator([gripper_name, apple_name])
+    observer = Observer()
+    Entity.set_observation_getter(observer.get_observation)
+
+    t_gripper = TiagoGripper(gripper_name)
+    apple = Apple(apple_name)
+
+    g1.bind(t_gripper)
+    o1.bind(apple)
     print(f"gripper: {g1}")
 
-    g1.bind(TiagoGripper())
-    o1.bind(Apple())
+    env.reset()
+    for i in range(10):
+        print(f"Step {i}")
+        obs = env.step([0])
+        observer.set_observation(obs)
+        print(f"Apple position: {apple.location}")
 
-    print(f"gripper: {g1}")
+        print(f"gripper.location: {g1.location}")
+        print(f"holding(gripper, apple): {g1.is_holding(o1)}")
 
-    print(r.decide())
-    print(r.evaluate())
+        print(f"Decision for Reach action: {r.decide()}")
+        print(f"Reward: {r.evaluate()}")
+        print("==========")
 
     # ve.bind(e)
 
