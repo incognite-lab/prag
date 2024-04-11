3d7db43964faea8447bb90f326f2b0c983d67075
radoskov <radoslav.skoviera@cvut.cz>
2024-04-11 18:02:23 +0200

    Reworked AA according to latest core

--- src/rddl/action.py
+++ src/rddl/action.py
@@ -1,36 +1,32 @@
 from abc import ABCMeta
 
-from rddl import Operand
-from rddl.predicate import Predicate
-from rddl.reward import Reward
+from rddl import Predicate, Reward
 
 
-class AtomicAction(Operand, metaclass=ABCMeta):
+class AtomicAction(Predicate, metaclass=ABCMeta):
     """Container for atomic action. AA is an operand (it can be evaluated or decided)
     and combines initial condition, goal (terminating) condition and reward function.
     """
 
-    def __init__(self) -> None:
+    def __init__(self, **kwds) -> None:
+        super().__init__(**kwds)
         self._predicate: Predicate
         self._reward: Reward
 
-    def decide(self):
+    def __call__(self):
         """ Decide whether goal condition is met.
 
         Returns:
             bool: True if goal condition is met, False otherwise.
         """
-        print(f"Checking action {str(self.__class__)}")
+        print(f"Checking action {str(self.__class__.__name__)}")
         return self._predicate.decide()
 
-    def evaluate(self):
+    def reward(self):
         """Evaluate reward function for the action.
 
         Returns:
             float: The reward value.
         """
-        print(f"Evaluating action {str(self.__class__)}")
+        print(f"Evaluating action {str(self.__class__.__name__)}")
         return self._reward.evaluate()
-
-    def __repr__(self) -> str:
-        return f"AA: {str(self.__class__)}"
